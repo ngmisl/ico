e0b2413cd0f24f6754dbff9d2b4a795189c2d9f4
ngmisl <98217124+ngmisl@users.noreply.github.com>
2022-07-12 17:45:28 +0200

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xd5F7e367126d404dFA5277D8F0E49fFA7AE34B92";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
